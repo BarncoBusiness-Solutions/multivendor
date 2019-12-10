3f85d0e851257d24dd951223612320c49a883b7d
abhismart8 <shuklaji88as@gmail.com>
2019-12-10 15:28:39 +0530

    Multivendor

---
+++ application/logs/log-2019-12-09.php
@@ -0,0 +1,102 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-12-09 06:03:34 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 06:03:43 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 06:03:44 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 06:03:46 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 06:03:51 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 06:08:02 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 06:08:44 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 06:08:49 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 06:08:50 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 06:23:54 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:43:09 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:43:30 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:43:34 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:43:36 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:43:42 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:45:47 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:45:47 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:45:47 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:45:47 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:45:47 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:45:48 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:45:48 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:47:21 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:50:58 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:54:02 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:54:04 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:54:12 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:54:18 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:54:23 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:54:28 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:54:40 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:54:43 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:54:50 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:54:55 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:54:58 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:55:05 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:55:10 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:55:24 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:55:31 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:55:34 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:55:34 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:55:40 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:55:43 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:55:45 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:55:50 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:55:51 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:56:39 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:56:42 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:56:42 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:56:42 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:56:50 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:56:50 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:56:50 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:57:10 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 07:57:33 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 08:06:10 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 08:06:10 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 08:06:33 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 08:06:33 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 08:10:18 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 08:10:18 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 08:10:18 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 08:10:23 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 08:10:23 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 08:10:23 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 08:13:22 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 08:13:23 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 08:13:23 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 08:19:35 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 08:19:35 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 08:19:35 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 08:19:35 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 08:19:35 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 08:19:36 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 08:19:36 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 08:24:10 --> Could not find the language line "vendor_home_page"
+ERROR - 2019-12-09 08:25:08 --> Severity: Notice --> Undefined index: brand_id C:\xampp\htdocs\shop\application\modules\vendor\models\Products_model.php 52
+ERROR - 2019-12-09 08:25:16 --> Could not find the language line "vendor_home_page"
+ERROR - 2019-12-09 10:08:45 --> Could not find the language line "vendor_home_page"
+ERROR - 2019-12-09 10:09:43 --> Could not find the language line "vendor_home_page"
+ERROR - 2019-12-09 10:09:58 --> Could not find the language line "vendor_home_page"
+ERROR - 2019-12-09 10:10:00 --> Could not find the language line "vendor_home_page"
+ERROR - 2019-12-09 10:24:01 --> Could not find the language line "vendor_home_page"
+ERROR - 2019-12-09 10:24:03 --> Could not find the language line "vendor_home_page"
+ERROR - 2019-12-09 10:27:30 --> Could not find the language line "vendor_home_page"
+ERROR - 2019-12-09 10:38:38 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 10:38:38 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 10:38:38 --> 404 Page Not Found: /index
+ERROR - 2019-12-09 10:39:00 --> Could not find the language line "vendor_home_page"
+ERROR - 2019-12-09 10:42:36 --> Mailer Error: SMTP connect() failed. https://github.com/PHPMailer/PHPMailer/wiki/Troubleshooting
+ERROR - 2019-12-09 10:44:24 --> Mailer Error: SMTP connect() failed. https://github.com/PHPMailer/PHPMailer/wiki/Troubleshooting
+ERROR - 2019-12-09 10:50:34 --> Could not find the language line "vendor_home_page"
+ERROR - 2019-12-09 10:50:36 --> Could not find the language line "vendor_home_page"
+ERROR - 2019-12-09 10:50:39 --> Could not find the language line "vendor_home_page"
+ERROR - 2019-12-09 10:50:57 --> Could not find the language line "vendor_home_page"
+ERROR - 2019-12-09 10:52:51 --> Could not find the language line "vendor_home_page"
+ERROR - 2019-12-09 10:52:54 --> Could not find the language line "vendor_home_page"
+ERROR - 2019-12-09 10:53:32 --> Could not find the language line "vendor_home_page"
+ERROR - 2019-12-09 10:53:36 --> Could not find the language line "vendor_home_page"
+ERROR - 2019-12-09 10:53:40 --> Could not find the language line "vendor_home_page"
---
+++ application/logs/log-2019-12-10.php
@@ -0,0 +1,9 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-12-10 10:31:19 --> Could not find the language line "vendor_home_page"
+ERROR - 2019-12-10 10:31:22 --> Could not find the language line "vendor_home_page"
+ERROR - 2019-12-10 10:31:26 --> Could not find the language line "vendor_home_page"
+ERROR - 2019-12-10 10:31:28 --> Could not find the language line "vendor_home_page"
+ERROR - 2019-12-10 10:31:30 --> Could not find the language line "vendor_home_page"
+ERROR - 2019-12-10 10:40:41 --> Could not find the language line "vendor_home_page"
+ERROR - 2019-12-10 10:40:42 --> Could not find the language line "vendor_home_page"

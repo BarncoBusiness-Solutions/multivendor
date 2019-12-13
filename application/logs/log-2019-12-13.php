2b0a96e5798ab3b6eed60cbe3ee6507422e52f37
abhismart8 <shuklaji88as@gmail.com>
2019-12-13 12:00:52 +0530

    log update

---
+++ application/logs/log-2019-12-13.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-12-13 07:23:40 --> Severity: Notice --> Undefined variable: arr C:\xampp\htdocs\shop\application\models\Public_model.php 520
